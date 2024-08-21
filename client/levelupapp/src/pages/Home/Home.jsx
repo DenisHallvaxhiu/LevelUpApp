7bdeab7f060af74bdfea76ced05df19d95dfac92
Denis Hallvaxhiu <dhallvaxhiu999@gmail.com>
2024-08-21 12:27:49 -0400

    Small structuring

---
+++ client/levelupapp/src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="sidebar left">
+        <h1>test</h1>
+      </div>
+      <div className="main-content">
+        {" "}
+        <h1>test</h1>
+      </div>
+      <div className="sidebar right">
+        {" "}
+        <h1>test</h1>
+      </div>
+    </div>
+  );
+}

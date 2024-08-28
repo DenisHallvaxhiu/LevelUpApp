568d0c148c4a6f3dac078b1c41d7dcf3a2d0df8c
Denis Hallvaxhiu <dhallvaxhiu999@gmail.com>
2024-08-28 00:16:22 -0400

    Home cards start of code

--- client/levelupapp/src/pages/Home/Home.jsx
+++ client/levelupapp/src/pages/Home/Home.jsx
@@ -1,13 +1,42 @@
 import React from "react";
-import "./Home.css"
+import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
-import Test from "../../assets/Working background.png"
+import Test from "../../assets/Working background.png";
 
 export default function Home() {
   return (
     <div className="Home">
       <Navbar></Navbar>
-      <img src={Test} alt=""  width={"100%"} height={"700px"}/>
+      <img src={Test} alt="" />
+      <div className="cards">
+        <div className="card">
+          <h2 className="card-title">Player Profile</h2>
+          <div className="circle-level">
+            <h1>9</h1>
+          </div>
+          <div className="bubbles"></div>
+          <div className="bubbles"></div>
+          <div className="bubbles"></div>
+          <div className="bubbles"></div>
+          <div className="bubbles"></div>
+          <div className="bubbles"></div>
+          <div className="bubbles"></div>
+          <div className="bubbles"></div>
+        </div>
+        <div className="card">
+          <h2 className="card-title">Player Profile</h2>
+        </div>
+        <div className="card">
+          <h2 className="card-title">Player Profile</h2>
+        </div>
+        <div className="card">
+          <h2 className="card-title">Player Profile</h2>
+        </div>
+        <div className="card">
+          <h2 className="card-title">Player Profile</h2>
+        </div>
+      </div>
+      <footer>SAdasds</footer>
     </div>
   );
 }
